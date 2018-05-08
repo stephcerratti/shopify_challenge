b4bf2d3c271cddb1379a818eb7e0bda84af605d0
Stephanie <stephanie.cerratti@gmail.com>
2018-05-08 17:54:04 -0400

    #03

--- script.js
+++ script.js
@@ -40,6 +40,18 @@ $(function() {
     type: 'POST',
     url: $(form).attr('action'),
     data: formData
+}).done(function(response) {
+    // Make sure that the formMessages div has the 'success' class.
+    $(formMessages).removeClass('error');
+    $(formMessages).addClass('success');
+
+    // Set the message text.
+    $(formMessages).text(response);
+
+    // Clear the form.
+    $('#name').val('');
+    $('#email').val('');
+    $('#message').val('');
 })
 });
 });
